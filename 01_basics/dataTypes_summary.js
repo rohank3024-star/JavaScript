0618a35dcfbdc065f0727a5462ff3cfe789c8616
rohank3024-star <rohank3024@gmail.com>
2026-02-17 17:27:30 +0000

    dataTypes_summary

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -24,3 +24,30 @@ const myFunction = function () {
     console.log("hello World"); 
 }
 console.log(typeof myFunction)
+
+
+// *****************
+
+
+// Stack (Primitive), Heap (Non Primitive or reference)
+
+let myName = "hola"
+let anotherName = myName
+anotherName = "chaiAurCode"
+
+//stack
+console.log(myName);
+console.log(anotherName);
+
+let  user1 = {
+    email: "user@google.com",
+    upi: "user1",
+}
+
+let user2 = user1
+
+user2.email = "xyz@google.com"
+
+//heap (by reference)
+console.log(user1.email);
+console.log(user2.email);
