3ecf48711cc59943ed1e33c4b3e7697f59a4d085
rohank3024-star <rohank3024@gmail.com>
2026-02-20 18:29:06 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 400
+
+const balance = new Number(300)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+const otherNum = 123.8666
+
+// console.log(otherNum.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ****** MATHS ******
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4, 8, 9, 6));
+// console.log(Math.max(4, 8, 9, 6));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
