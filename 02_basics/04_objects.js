cb7474e27852b56149950ceadefdeef72ae5304b
rohank3024-star <rohank3024@gmail.com>
2026-02-21 16:21:20 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,12 +47,47 @@ const users = [
 ]
 
 
-console.log(users[1].email)
+// console.log(users[1].email)
 
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    courseName: "Js in hindi",
+    price: "999",
+    courseInstructor: "Aloo"
+}
+
+// course.courseInstructor
+console.log(course.courseInstructor)
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor)
+console.log(instructor)
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "google")
+
+
+
+// {
+//     "name": "rohan",
+//     "courseName": "Js in Hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
