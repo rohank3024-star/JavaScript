2d25c8ba754bc3907d693db98663898e30084873
rohank3024-star <rohank3024@gmail.com>
2026-02-28 15:05:30 +0000

    switch

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,46 @@
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+
+//     default:
+//         console.log("Default Case Matched");
+//         break;
+// }
+const month = "march"
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default Case Matched");
+        break;
+}
