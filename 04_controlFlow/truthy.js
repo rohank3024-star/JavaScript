c72d29b58ad1e8c443c6ea8d8fa6deec6fd90c92
rohank3024-star <rohank3024@gmail.com>
2026-02-28 15:23:23 +0000

    truth and false , terniary operator

---
+++ 04_controlFlow/truthy.js
@@ -0,0 +1,32 @@
+const userEmail = "rk@email.in"
+
+// if(userEmail){
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+//Falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//Truthy Values
+// "0", 'false', " ", [], {}, function(){}
+
+const email = []
+if (email.length === 0) {
+    console.log("Array is Empty");
+}
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is Empty");
+}
+
+//Terniary Operator
+
+//condition ? true : false
+
+const iceteaPrice = 100
+
+iceteaPrice >= 80 ? console.log("less than 100") : console.log("greater than 100");
