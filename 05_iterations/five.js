bce46d02fb609d27699fb8909500f93e055a0542
rohank3024-star <rohank3024@gmail.com>
2026-05-13 13:49:56 +0000

    higher order loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,37 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "python",
+        languageFileName: "python"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+]
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})
